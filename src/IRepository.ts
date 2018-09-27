d375a2796a61703833c17734640098b1a06b163e
Ehsan <e.hosseini.ir@gmail.com>
2018-09-27 11:11:03 +0330

    reformat

--- src/IRepository.ts
+++ src/IRepository.ts
@@ -1,10 +1,14 @@
-interface IRepository <T, PK>{
+interface IRepository<T, PK> {
 
-	get(primaryKey: PK):Promise<T>;
-	add(model: T): Promise<T>;
-	edit(model:T): Promise<T>;
-	delete(model:T|PK): Promise<T|boolean>;
-	search(mongoQuery:Object, skip: number, limit: number): Promise<T[]>;
+	get (primaryKey: PK): Promise<T>;
+
+	add (model: T): Promise<T>;
+
+	edit (model: T): Promise<T>;
+
+	delete (model: T | PK): Promise<T | boolean>;
+
+	search (mongoQuery: object, skip: number, limit: number): Promise<T[]>;
 
 }
 
